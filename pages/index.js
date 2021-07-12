ac2df9d0a2c08c04ad7482386904a58ddac66b17
aloisiosousa <32015500+aloisiosousa@users.noreply.github.com>
2021-07-12 20:36:42 -0300

    Editando o css do body e divs

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Title = styled.h1 `
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1 `
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
 
+const Box = styled.div `
+  background: #ffffff;
+  border-radius: 8px;
+  color: red;
+`;
 export default function Home() {
     return (
-
-    )
+        <main>
+            <Box> 
+                Imagem
+            </Box>
+            <Box> 
+                Bem Vindo
+            
+            </Box>
+            <Box> 
+                Comunidades
+            </Box>
+            <Box> 
+                
+            </Box>
+        
+            
+        </main>
+    );
 }
