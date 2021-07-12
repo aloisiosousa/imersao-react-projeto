b4da9ccb2edfdeefae7c18d9f4d043767221abcf
aloisiosousa <32015500+aloisiosousa@users.noreply.github.com>
2021-07-12 18:41:21 -0300

    Enviando o inicio da projeto - alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
+const Title = styled.h1 `
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+    return <Title > Minha primeira página React.js Next.js < /Title>
 }
